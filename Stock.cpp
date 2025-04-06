ca05fe88da16d499a919cac8bccc7a5c94501f14
NikitaVesnin <NekitMagik@yandex.ru>
2025-04-06 20:22:45 +0300

    Решение задания 1

---
+++ Stock.cpp
@@ -0,0 +1,34 @@
+#include "Stock.h"
+#include <iostream>
+using namespace std;
+
+Person::Person()
+{
+ lastName = "";
+ firstName = "";
+ patronymic = "";
+}
+
+Person::Person(const string& ln, const string& fn, const string& pn)
+{
+ lastName = ln;
+ firstName = fn;
+ patronymic = pn;
+}
+
+void Person::Show()
+{
+ cout << lastName << " " << firstName << endl;
+}
+
+void Person::ShowFormal()
+{
+ if (patronymic.empty())
+ {
+  cout << lastName << " " << firstName << endl;
+ }
+ else
+ {
+  cout << lastName << " " << firstName << " " << patronymic << endl;
+ }
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "Stock.h"
+#include <iostream>
+#include <locale>
+
+int main()
+{
+ Person person("Vesnin", "Nikita", "Konstantinovich");
+ person.Show();
+ person.ShowFormal();
+ return 0;
+}
