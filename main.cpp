8d3ee082eba2e110efdb541d16d77fe6085a3231
NikitaVesnin <NekitMagik@yandex.ru>
2025-06-01 22:49:45 +0300

    Решение задания 2

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include "RightTriangle.h"
 #include <iostream>
-#include <locale>  
-
+#include "RightTriangle.h"
 int main() {
     try {
         
         RightTriangle triangle(3.0, 4.0);
-        
-        std::cout << "Катеты: " << triangle.getA() << " и " << triangle.getB() << std::endl;
+        std::cout << triangle.toString() << std::endl;
         std::cout << "Площадь: " << triangle.calculateArea() << std::endl;
-        std::cout << "Радиус описанной окружности: " << triangle.calculateCircumradius() << std::endl;
-
-    } catch (const std::invalid_argument& e) {
+        std::cout << "Гипотенуза: " << triangle.calculateHypotenuse() << std::endl;
+        std::cout << "Радиус описанной окружности: " 
+                  << triangle.calculateCircumradius() << std::endl;
+        RightTriangle invalidTriangle(-1, 0); 
+    }
+    catch (const std::exception& e) {
         std::cerr << "Ошибка: " << e.what() << std::endl;
         return 1;
     }
